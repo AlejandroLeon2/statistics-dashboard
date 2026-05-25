a87bf24bca560fa8c08e197fcd697ea031a43e0c
RobertContrerasDev <rcontrerasmamani@gmail.com>
2026-05-24 22:20:51 -0500

    se creo la funcion js de ventas por Categoria y se implementó en el dashboardPage.html

--- src/scripts/ventasCategoria.js
+++ src/scripts/ventasCategoria.js
@@ -1,7 +1,51 @@
-import { datosCrudos } from "../data/datosCrudos";
+import { datosCrudos } from "../data/datosCrudos.js";
 
 const ventas = datosCrudos.ventas;
 
 function getVentasPorCategoria(ventas){
 
+    const acumulador = {};
+
+    ventas.forEach(ventas => {
+
+        const categoria = ventas.categoria;
+        const monto = ventas.monto;
+
+        if(acumulador[categoria]){
+            acumulador[categoria]+= monto;
+
+        }else{
+            acumulador[categoria] = monto;
+        }
+        
+    });
+
+    return{
+        labels:Object.keys(acumulador),
+        data:Object.values(acumulador)
+    };
 }
+
+document.addEventListener("DOMContentLoaded",() =>{
+    const resultado = getVentasPorCategoria(ventas);
+
+    const ctx = document.getElementById("chartCategoria");
+
+    new Chart(ctx,{
+        type:"bar",
+        data:{
+            labels:resultado.labels,
+
+            datasets:[{
+                label: "Ventas por Categoria",
+                data : resultado.data,
+                backgroundColor:[
+                    "orange",
+                    "skyblue"
+                ]
+            }]
+
+        },
+
+    });
+});
